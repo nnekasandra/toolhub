0a7c43dc5a6be69d005e5be013f08f36647afdf5
nnekasandra <nnekasandra@gmail.com>
2022-10-26 04:32:05 +0100

    Edit contents for tailwind configuration
    Edit content to ensure the tailwind css styling reaches the entire page or components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/App.js", "./public/index.html"],
+  content: ["./src/App.js", "./public/index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
